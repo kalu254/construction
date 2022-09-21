dd65c2c0b1b4c01dd50ddabaa75657ed144eb6af
Masterjohn <kamau.john.mwaniki@gmail.com>
2022-09-21 21:46:37 +0300

    made some changes

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -67,4 +67,4 @@ List<Product> products = [
 ];
 
 String dummyText =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. When an unknown printer took a galley.";
+    "Make your order today and money by getting best offers.";
